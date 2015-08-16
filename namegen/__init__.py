4964ff7a14b23255f6fca1b150ed81e40ae91857
Jamie Todd Rubin <jamie@jamietoddrubin.com>
2015-08-16 16:19:53 -0400

    Added geo data and functions:
    * Can now randomly select cities (from US Census list) by state and/or population.

--- namegen/__init__.py
+++ namegen/__init__.py
@@ -80,6 +80,42 @@ def has_diminutive(name):
     else:
         return ''
 
+def state(region):
+    this_dir, this_file = os.path.split(__file__)
+    DATA_PATH = os.path.join(this_dir, "data/geo/states.txt")
+
+    states = []
+    with open(DATA_PATH, 'r') as f:
+        for line in f:
+            tokens = line.rstrip('\n').split(',')
+            if region == '':
+                states.append(tokens[0])
+            elif region == tokens[1]:
+                states.append(tokens[0])
+
+    return random.choice(states)
+
+
+def city(statename, population):
+    this_dir, this_file = os.path.split(__file__)
+    DATA_PATH = os.path.join(this_dir, "data/geo/cities.txt")
+
+    if statename == '':
+        statename = state('')
+
+    cities = []
+    with open(DATA_PATH, 'r') as f:
+        for line in f:
+            tokens = line.rstrip('\n').split(',')
+            if tokens[1] == statename:
+                if population == 0:
+                    cities.append(tokens[0])
+                elif population > int(tokens[2]):
+                    cities.append(tokens[0])
+
+    return random.choice(cities)
+
+
 def get_max_population(year, gender):
     # 1. Find the right file
     this_dir, this_file = os.path.split(__file__)

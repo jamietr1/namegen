b9a4f632f2d36b3870c04648c0ce167961f90eed
Jamie Todd Rubin <jamie@jamietoddrubin.com>
2015-08-21 19:58:01 -0400

    Cached city, state, firstname, and surname data on inital load
    The data for cities, states, and player names is now cached on initial load of the module. This speeds things up dramatically, as the files only need to be read once.

--- namegen/person.py
+++ namegen/person.py
@@ -6,27 +6,24 @@ def get_random_firstname(year, gender):
     if year < 1880 or year > 2014:
         return ''
 
-    # 1. Find the right file
-    this_dir, this_file = os.path.split(__file__)
-    DATA_PATH = os.path.join(this_dir, "data/firstnames", 'yob' + str(year) + '.txt')
-
-    # 2. Build a mapping
     min_val = 0
-    max_val = get_max_population(year, gender)
+    if gender == 'M':
+        max_val = 108252256
+    else:
+        max_val = 77608249
+
     roll = random.randint(1, max_val)
 
-    with open(DATA_PATH, 'r') as f:
-        for line in f:
-            splitLine = line.rstrip('\n').split(',')
-            if splitLine[1] == gender:
-                max_val = int(splitLine[2])
-                if (roll > min_val) and (roll <= max_val):
-                    first_name = splitLine[0]
-                    break
+    for record in firstname_list:
+        fields = record.split(',')
+        if fields[1] == gender:
+            max_val = int(fields[2])
+            if (roll > min_val) and (roll <= max_val):
+                first_name = fields[0]
+                break
+            min_val = max_val
 
-                min_val = max_val
-
-    # 3. Does the name have a diminutive?
+    # Does the name have a diminutive?
     diminutives = get_random_diminutive(first_name)
     if (diminutives != ''):
         if random.randint(1,100) > 50:
@@ -40,14 +37,10 @@ def get_random_firstname(year, gender):
 
 def get_random_diminutive(name):
     name_dict = {}
-    this_dir, this_file = os.path.split(__file__)
-    DATA_PATH = os.path.join(this_dir, "data/firstnames", 'diminutives.txt')
 
-    with open(DATA_PATH, 'r') as f:
-        for line in f:
-            line.rstrip('\n')
-            splitLine = line.split(':')
-            name_dict[splitLine[0]] = splitLine[1]
+    for line in diminutive_list:
+        splitLine = line.split(':')
+        name_dict[splitLine[0]] = splitLine[1]
 
     if name in name_dict:
         return name_dict[name].rstrip('\n')
@@ -55,21 +48,20 @@ def get_random_diminutive(name):
         return ''
 
 def get_random_surname():
-    this_dir, this_file = os.path.split(__file__)
-    DATA_PATH = os.path.join(this_dir, "data/surnames", 'surnames.txt')
-
     min_val = 0
     max_val = 60113
     roll = random.randint(1, max_val)
 
-    with open(DATA_PATH, 'r') as f:
-        for line in f:
-            splitLine = line.rstrip('\n').split(',')
-            max_val = int(splitLine[1])
-            if (roll > min_val) and (roll <= max_val):
-                surname = splitLine[0]
 
-            min_val = max_val
+    for line in surname_list:
+        splitLine = line.rstrip('\n').split(',')
+        max_val = int(splitLine[1])
+        if (roll > min_val) and (roll <= max_val):
+            surname = splitLine[0]
+            break
+
+        min_val = max_val
+
     return surname.title()
 
 def get_random_fullname(year, gender):
@@ -77,16 +69,57 @@ def get_random_fullname(year, gender):
     last_name = get_random_surname()
     return first_name, last_name
 
-def get_max_population(year, gender):
-    # 1. Find the right file
-    this_dir, this_file = os.path.split(__file__)
-    DATA_PATH = os.path.join(this_dir, "data/firstnames", 'yob' + str(year) + '.txt')
-
+def get_max_population(year, gender, namepart):
     population = []
-    with open(DATA_PATH, 'r') as f:
-        for line in f:
-            splitLine = line.rstrip('\n').split(',')
-            if splitLine[1] == gender:
-                population.append(int(splitLine[2]))
+    if namepart == "firstname":
+        for record in firstname_list:
+            fields = record.split(',')
+            if fields[1] == gender:
+                population.append(int(fields[2]))
+    else:
+        for record in lastname_list:
+            fields = record.split(',')
+            if fields[1] == gender:
+                population.append(int(fields[2]))
 
     return max(population)
+
+def get_firstnames():
+    global firstname_list
+    global max_population_m
+    global max_population_f
+    firstname_list = []
+    max_population_m = {}
+    max_population_f = {}
+    this_dir, this_file = os.path.split(__file__)
+
+    for year in range(1880, 2014):
+        DATA_PATH = os.path.join(this_dir, "data/firstnames", 'yob' + str(year) + '.txt')
+        with open(DATA_PATH, 'r') as f:
+            for line in f:
+                line = line.rstrip('\n')
+                firstname_list.append(line)
+
+def get_diminutives():
+    global diminutive_list
+    diminutive_list = []
+
+    this_dir, this_file = os.path.split(__file__)
+    DATA_PATH = os.path.join(this_dir, "data/firstnames", 'diminutives.txt')
+
+    with open(DATA_PATH, 'r') as f:
+        for line in f:
+            line = line.rstrip('\n')
+            diminutive_list.append(line)
+
+def get_surnames():
+    global surname_list
+    surname_list = []
+
+    this_dir, this_file = os.path.split(__file__)
+    DATA_PATH = os.path.join(this_dir, "data/surnames", 'surnames.txt')
+
+    with open(DATA_PATH, 'r') as f:
+        for line in f:
+            line = line.rstrip('\n')
+            surname_list.append(line)
